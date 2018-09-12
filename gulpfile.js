cd7c520f149ed9a26194efdf41c20d9084847bbe
Nicolas <ngaribaldif@gmail.com>
2018-09-12 18:51:10 -0300

    added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
 
 gulp.task('default',function(){
    console.log('Gulp is installed'); 
+});
+
+gulp.task('watch',function(){
+    watch('./index.html',function(){
+        gulp.start('default');
+    })
 });
